959a024a74a3b93cf7bc020f0d81debe6f26a94d
Deserdhod <32869477+Deserdhod@users.noreply.github.com>
2018-05-06 15:25:34 +0700

    1

--- core/src/com/mygdx/game/PlayStates/DropStartMenuState.java
+++ core/src/com/mygdx/game/PlayStates/DropStartMenuState.java
@@ -1,6 +1,9 @@
 package com.mygdx.game.PlayStates;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.Game;
 import com.mygdx.game.States.State;
 import com.mygdx.game.States.StateManager;
 
@@ -10,23 +13,39 @@ import com.mygdx.game.States.StateManager;
 
 public class DropStartMenuState extends State {
 
+    private Texture backGround;
+
     public DropStartMenuState(StateManager gsm) {
         super(gsm);
+
+        backGround = new Texture("ds.png");
+        camera.setToOrtho(false, Game.WIDTH, Game.HEIGTH);
+
     }
+
 
     @Override
     protected void handle() {
 
+        if (Gdx.input.justTouched())
+            gsm.push(new DropState(gsm));
     }
 
     @Override
     public void update(float dt) {
-
+        handle();
     }
 
     @Override
     public void render(SpriteBatch sb) {
 
+        sb.setProjectionMatrix(camera.combined);
+
+        sb.begin();
+
+        sb.draw(backGround, 0, 0, Game.WIDTH, Game.HEIGTH);
+
+        sb.end();
     }
 
     @Override
---
+++ core/src/com/mygdx/game/PlayStates/DropState.java
@@ -0,0 +1,36 @@
+package com.mygdx.game.PlayStates;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.States.State;
+import com.mygdx.game.States.StateManager;
+
+/**
+ * Created by bgk_0_000 on 06.05.2018.
+ */
+
+public class DropState extends State {
+
+    public DropState(StateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    protected void handle() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
